55c49153ca5a3b4e8d7f86b64d371ef20eb4f783
Andrei Alexeyev <akari@taisei-project.org>
2019-09-15 21:26:29 +0300

    find-dylib-name.py: fix python3.5 compat for the last god damn time

--- find-dylib-name.py
+++ find-dylib-name.py
@@ -19,7 +19,7 @@ def main(argv):
     verbose('cc:', cc)
 
     o = subprocess.run(cc + ['-print-search-dirs'], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    libdirs = re.search(r'[\^\n]libraries: =(.*)', o.stdout.decode('utf-8'))[1].strip().split(os.pathsep)
+    libdirs = re.search(r'[\^\n]libraries: =(.*)', o.stdout.decode('utf-8')).group(1).strip().split(os.pathsep)
 
     verbose('Search path:\n\t' + '\n\t'.join(libdirs))
 
@@ -30,14 +30,14 @@ def main(argv):
             p = p.resolve()
             verbose('Real path', p)
             verbose('Name', p.name)
-            dlname = re.search(r'(.*?\.so(?:\.[^.]+)?)', p.name)[0]
+            dlname = re.search(r'(.*?\.so(?:\.[^.]+)?)', p.name).group(0)
             verbose('Reduced name', dlname)
             assert p.with_name(dlname).resolve() == p
             print(dlname)
             quit(0)
 
-
     quit(1)
+
 
 if __name__ == '__main__':
     main(sys.argv)
